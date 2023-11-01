848d5c0982c718902588ca3ff342137375bb0275
Jaseetha <59699552+Jaseetha@users.noreply.github.com>
2023-11-01 16:02:57 +0530

    Create prerequeries.sh

---
+++ Kubernetes/prerequeries.sh
@@ -0,0 +1,7 @@
+az ad sp create-for-rbac --name dr-iac-sp --role Contributor --scopes /subscriptions/<subscription_id>
+
+export ARM_SUBSCRIPTION_ID="<azure_subscription_id>"
+export ARM_TENANT_ID="<azure_subscription_tenant_id>"
+export ARM_CLIENT_ID="<service_principal_appid>"
+export ARM_CLIENT_SECRET="<service_principal_password>"
+
